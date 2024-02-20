c931a8abceac89edc961466a4dc4b09770ad8936
natdm02 <natalidimarino2002>
2024-02-20 16:00:21 +0100

    [add]variabili km e età

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// variabile di km-età
+const chilometri = parseInt(prompt('Quanti chilometri vorresti percorrere?', 100));
+const age        = parseInt(prompt('Quanti anni hai?', 100));
+const price      = 0.21;
+
+console.log(chilometri, age);
+
+let discount = 0;
+
